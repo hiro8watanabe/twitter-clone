40121da1eefa6f910aace449ceef4e6f246194e8
hiro <>
2022-06-27 15:51:41 +0900

    Add sidebar props & style

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,14 +1,35 @@
 import React from "react";
 import TwitterIcon from "@mui/icons-material/Twitter";
+import SidebarOption from "./SidebarOption";
+import HomeIcon from "@mui/icons-material/Home";
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
+import ListAltIcon from "@mui/icons-material/ListAlt";
+import PermIdentityIcon from "@mui/icons-material/PermIdentity";
+import { Button } from "@mui/material";
+import "./Sidebar.css";
 
 function Sidebar() {
   return (
     <div className="sidebar">
       {/**ツイッターアイコン*/}
-      <TwitterIcon className="sidebar_twitterIcon"/>
+      <TwitterIcon className="sidebar_twitterIcon" />
 
       {/**sidebarOption*/}
+      <SidebarOption icon={<HomeIcon />} text="ホーム" />
+      <SidebarOption icon={<SearchIcon />} text="話題を検索" />
+      <SidebarOption icon={<NotificationsNoneIcon />} text="通知" />
+      <SidebarOption icon={<MailOutlineIcon />} text="メッセージ" />
+      <SidebarOption icon={<BookmarkBorderIcon />} text="ブックマーク" />
+      <SidebarOption icon={<ListAltIcon />} text="リスト" />
+      <SidebarOption icon={<PermIdentityIcon />} text="プロフィール" />
+      <SidebarOption icon={<PermIdentityIcon />} text="もっと見る" />
       {/**ツイートボタン*/}
+      <Button variant="outlined" className="sidebar_tweet">
+        ツイートする
+      </Button>
     </div>
   );
 }
---
+++ src/components/sidebar/SidebarOption.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./SidebarOption.css";
+
+function SidebarOption(props) {
+  const { text, icon } = props;
+  return (
+    <div className="sidebar_option">
+      {icon}
+      <h2>{text}</h2>
+    </div>
+  );
+}
+
+export default SidebarOption;
