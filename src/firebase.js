8294b7edfea6823f5c29b6d563c96f3da00156fa
hiro <>
2022-06-28 16:13:48 +0900

    initialize Firebase

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAfQrEd9KmnIFVfHKGOmXN6sBT6daiwlE8',
+  authDomain: 'twitter-clone-c4625.firebaseapp.com',
+  projectId: 'twitter-clone-c4625',
+  storageBucket: 'twitter-clone-c4625.appspot.com',
+  messagingSenderId: '35121052777',
+  appId: '1:35121052777:web:a24c4555df8a48b97bb6f8',
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export default db;
